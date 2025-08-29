4bd49b6bd041961e11f026d3c0988815f933e2ba
Markimg22 <marquim.net.221200@hotmail.com>
2025-08-28 20:51:41 -0400

    exercicio 4 e 7

---
+++ aula-2/exercicio-4.py
@@ -0,0 +1,10 @@
+nota_1: int = int(input("Digite a 1º nota: "))
+nota_2: int = int(input("Digite a 2º nota: "))
+nota_3: int = int(input("Digite a 3º nota: "))
+nota_4: int = int(input("Digite a 4º nota: "))
+
+media: int = (nota_1 + nota_2 + nota_3 + nota_4) / 4
+
+print(f"Sua média é: {media} parabéns.")
+# print("Sua média é: ", media, " parabéns")
+# print("Sua média é: " + str(media) + " parabéns")
